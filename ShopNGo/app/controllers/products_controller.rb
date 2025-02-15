20957207d917bff38c8e5d37f450824a2508ca7e
Nishchay Gupta <nishchay.gupta@berylsystems.com>
2025-02-15 13:35:27 +0530

    Added the controller and routes

---
+++ ShopNGo/app/controllers/products_controller.rb
@@ -0,0 +1,21 @@
+class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :stock, :description, :available, :category)
+  end
+  
+end
