3315c7e391b729fd09ac60c5271d10ead63816c5
Nishchay Gupta <nishchay.gupta@berylsystems.com>
2025-02-24 18:19:05 +0530

    Learn acceptance validations

--- Company/app/models/employee.rb
+++ Company/app/models/employee.rb
@@ -1,6 +1,12 @@
 class Employee < ApplicationRecord
   belongs_to :department
   attr_accessor :accept_terms
-  validates :name, :gender, :address, :department_id, presence: true
-  validates :accept_terms, acceptance: true , on: :create 
+  
+  # validates :name, :gender, :address, :department_id, presence: true
+
+# ACCEPTANCE VALIDATIONS
+  validates :accept_terms, presence: true
+  validates :accept_terms, acceptance: { message: "must be abided" }
+  validates :accept_terms, acceptance: { accept: "1", message: 'must be checked' }
+
 end
